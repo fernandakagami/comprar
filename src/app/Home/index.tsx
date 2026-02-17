b852f240327a268c8f16c4b1ef0bb6b64ba7c8d0
Fernanda <fernandakagami@gmail.com>
2026-02-17 16:53:05 -0300

    Add content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,13 +3,19 @@ import { View, Image } from 'react-native';
 
 import { Button } from '@/components/Button';
 import { styles } from './styles';
+import { Input } from '@/components/Input';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
      
-      <Button title="Entrar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   );
 }
